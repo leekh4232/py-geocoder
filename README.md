9f0422522e383e12454ceaeb46735e54c360860a
이광호 <leekh4232@gmail.com>
2024-05-17 17:36:04 +0900

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # py-geocoder
+
 브이월드 OpenAPI와 연동하는 지오코더 프로그램
+
+
+![Badge](https://img.shields.io/badge/Author-Lee%20KwangHo-blue.svg?style=flat-square&logo=appveyor) &nbsp;
+![Badge](https://img.shields.io/badge/Author-Ju%20YoungA-pink.svg?style=flat-square&logo=appveyor) &nbsp;
+![Generic badge](https://img.shields.io/badge/version-0.0.1-critical.svg?style=flat-square&logo=appveyor) &nbsp;
+[![The MIT License](https://img.shields.io/badge/license-MIT-orange.svg?style=flat-square&logo=appveyor)](http://opensource.org/licenses/MIT) &nbsp;
+![Python](https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=appveyor) &nbsp;
+![Pandas](https://img.shields.io/badge/Pandas-150458?style=flat-square&logo=appveyor) &nbsp;
+![SciPy](https://img.shields.io/badge/SciPy-8CAAE6?style=flat-square&logo=appveyor) &nbsp;
+![Scikit-learn](https://img.shields.io/badge/scikit-learn-F7931E?style=flat-square&logo=appveyor)
+
+## Hello World
+
+이 자료는 메가스터디IT아카데미에서 진행중인 산대특 빅데이터 분석 과정의 수업 자료로 사용되기 위해 작성된 소스코드 입니다.
+
+학생 배포 용이를 위해 공개저장소 형태로 배포되지만 사용 과정에서의 질문은 오프라인 수업을 통해서만 받습니다.
+
+MIT 라이센스를 따릅니다.
+
+
+## Installation
+
+`pip` 명령으로 `setup.py` 파일이 있는 위치를 지정합니다.
+
+### [1] Remote Repository
+
+```shell
+pip install --upgrade git+https://github.com/leekh4232/hossam-data-helper.git
+```
+
+or
+
+```shell
+pip install --upgrade git+ssh://git@github.com:leekh4232/hossam-data-helper.git
+```
+
+
+
+### [2] Local Repository
+
+```shell
+pip install --upgrade git+file:///path/to/your/git/project/
+```
+
+### [3] Local Directory
+
+```shell
+pip install --upgrade /path/to/your/project/
+```
+
+
+## Uninstallation
+
+```shell
+pip uninstall -y hossam
+```
+
+
+## Documentation
+
+[Documentation](https://leekh4232.github.io/hossam-data-helper/hossam)
---
+++ py_geocoder/main.py
@@ -0,0 +1,139 @@
+import typer
+import requests
+import json
+import concurrent.futures as futures
+from os import path
+from datetime import datetime as dt
+from pandas import read_excel, read_csv, DataFrame
+from tqdm import tqdm
+import time
+
+app = typer.Typer()
+
+
+
+def check_path(input: str, output: str = None) -> tuple[str, str, str]:
+    input = path.abspath(input)
+    
+    if output is None:
+        output = path.splitext(input)[0].replace(' ', '_') + '_geocoded' + path.splitext(input)[1]
+    
+    typer.echo("입력파일: %s" % input)
+    typer.echo("출력파일: %s" % output)
+
+    if not path.exists(input):
+        raise FileNotFoundError("입력 파일이 존재하지 않습니다.")
+
+    return input, output
+
+
+
+def get_dataframe(input: str, addr: str) -> DataFrame:
+    extname: str = path.splitext(input)[1].lower()
+
+    if extname == '.csv':
+        df: DataFrame = read_csv(filepath_or_buffer=input, encoding='utf-8')
+    elif extname == '.xlsx':
+        df: DataFrame = read_excel(io=input)
+    else:
+        raise ValueError("지원하지 않는 파일 형식입니다.")
+    
+    typer.echo("주소필드: %s" % addr)
+    
+    if addr not in df.columns:
+        raise ValueError("입력파일에 주소필드가 존재하지 않습니다.")
+    
+    return df
+
+
+def geocode_item(index: int, addr: str, key: str) -> dict:
+    time.sleep(0.1)
+
+    if not addr:
+        raise ValueError("주소가 존재하지 않습니다.")
+
+    url: str = f"https://api.vworld.kr/req/address?service=address&request=getCoord&key={key}&address={addr}&type=ROAD&format=json"
+    response: requests.Response = requests.get(url)
+
+    if response.status_code != 200:
+        raise requests.exceptions.RequestException("API 요청에 실패했습니다.")
+
+    response.encoding = 'utf-8'
+    result = response.json()
+
+    status = result['response']['status']
+
+    if status == 'ERROR':
+        error_code = result['response']['error']['code']
+        error_text = result['response']['error']['text']
+        raise requests.exceptions.RequestException(f"[{error_code}] {error_text}")
+
+    longitude = result['response']['result']['point']['x']
+    latitude = result['response']['result']['point']['y']
+    result: tuple[int, Any, Any] =  (index, latitude, longitude)
+    #print(result)
+    return result
+
+
+def geocode_process(df: DataFrame, addr: str, key: str) -> DataFrame:
+    data: DataFrame = df.copy()
+    size: int = len(data)
+    success = 0
+    processes = []
+
+    typer.echo("------------------------------------------")
+
+    with tqdm(total=size, colour="yellow") as pbar:
+        with futures.ThreadPoolExecutor(max_workers=10) as executor:
+            for i in range(size):
+                address: str = str(data.loc[i, addr]).strip()
+                
+                if address == 'nan':
+                    pbar.update(1)
+                    continue
+
+                processes.append(
+                    executor.submit(
+                        geocode_item, index=i, addr=address, key=key
+                    )
+                )
+
+            for p in futures.as_completed(processes):
+                # 각 회귀 함수의 결과값(회귀모형 객체)을 저장한다.
+                index, latitude, longitude = p.result()
+                data.loc[index, 'latitude'] = latitude
+                data.loc[index, 'longitude'] = longitude
+                pbar.update(1)
+                success += 1
+
+    data['latitude'] = data['latitude'].astype(float)
+    data['longitude'] = data['longitude'].astype(float)
+
+    typer.echo("------------------------------------------")
+    typer.echo(f"총 {size}개의 데이터 중 {success}개의 데이터가 성공적으로 처리되었습니다.")
+
+    return data
+
+
+@app.command()
+def main(key: str = typer.Option(), input: str = typer.Option(), output: str = None, addr: str = 'ADDR') -> None:
+    """
+    -----------\n
+    브이월드 OpenAPI와 연동하여 GeoCoding을 수행하고 결과를 파일로 저장합니다.\n \n
+    -----------\n
+    Args:\n
+        - key (str): 브이월드에서 발급받은 API KEY\n
+        - input (str): 데이터 파일의 경로 (Excel or UTF-8 CSV)\n
+        - output (str, optional): 결과 파일의 경로. Defaults to None.\n
+        - addr (str, optional): 주소 필드 이름. Defaults to 'ADDR'.\n
+    -----------\n
+    """
+    input, output = check_path(input=input, output=output)
+    df: DataFrame = get_dataframe(input=input, addr=addr)
+    result: DataFrame = geocode_process(df=df, addr=addr, key=key)
+
+    result.to_excel(output, index=False)
+    
+
+if __name__ == "__main__":
+    app()
