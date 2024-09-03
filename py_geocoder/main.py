8da28c43cc2549bf335d2a6b13321a9e57a6adec
leekh <leekh4232@gmail.com>
2024-09-03 17:52:18 +0900

    비동기 처리 및 예외처리 개선

--- py_geocoder/main.py
+++ py_geocoder/main.py
@@ -7,88 +7,106 @@ from datetime import datetime as dt
 from pandas import read_excel, read_csv, DataFrame
 from tqdm import tqdm
 import time
+import logging as logger
 
 app = typer.Typer()
 
+logfileName = dt.now().strftime("py-geocoder-%Y%m%d-%H%M%S.log")
+logger.basicConfig(
+    filename=logfileName,
+    level=logger.INFO,
+    encoding="utf-8",
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="[%m/%d/%Y %I:%M:%S %p]",
+)
 
 
 def check_path(input: str, output: str = None) -> tuple[str, str, str]:
     input = path.abspath(input)
 
     if output is None:
-        output = path.splitext(input)[0].replace(' ', '_') + '_geocoded' + path.splitext(input)[1]
+        output = (
+            path.splitext(input)[0].replace(" ", "_")
+            + "_geocoded"
+            + path.splitext(input)[1]
+        )
 
     typer.echo("입력파일: %s" % input)
     typer.echo("출력파일: %s" % output)
 
     if not path.exists(input):
-        raise FileNotFoundError("입력 파일이 존재하지 않습니다.")
+        err = FileNotFoundError(
+            "[FileNotFoundError] 입력 파일이 존재하지 않습니다. (%s)" % input
+        )
+        raise err
 
     return input, output
-
 
 
 def get_dataframe(input: str, addr: str) -> DataFrame:
     extname: str = path.splitext(input)[1].lower()
 
-    if extname == '.csv':
-        df: DataFrame = read_csv(filepath_or_buffer=input, encoding='utf-8')
-    elif extname == '.xlsx':
+    if extname == ".csv":
+        df: DataFrame = read_csv(filepath_or_buffer=input, encoding="utf-8")
+    elif extname == ".xlsx":
         df: DataFrame = read_excel(io=input)
     else:
-        raise ValueError("지원하지 않는 파일 형식입니다.")
+        raise ValueError("[ValueError] 지원하지 않는 파일 형식입니다.")
 
     typer.echo("주소필드: %s" % addr)
 
     if addr not in df.columns:
-        raise ValueError("입력파일에 주소필드가 존재하지 않습니다.")
+        raise ValueError("[ValueError] 입력파일에 주소필드가 존재하지 않습니다.")
 
     return df
 
 
 def geocode_item(session, index: int, addr: str, key: str) -> dict:
-    time.sleep(0.5)
-
-    if not addr:
-        raise ValueError("[Error] 주소가 존재하지 않습니다. (%d) -> %s" % (index, addr))
+    if not addr or addr == "nan":
+        raise ValueError(
+            "[Warning] 주소가 존재하지 않습니다. (%d) -> %s" % (index, addr)
+        )
 
     url: str = f"https://api.vworld.kr/req/address"
     params = {
-        "service" : "address",
-        "request" : "getCoord",
+        "service": "address",
+        "request": "getCoord",
         "key": key,
-        "address" : addr,
-        "type" : "ROAD",
-        "format" : "json"
+        "address": addr,
+        "type": "ROAD",
+        "format": "json",
     }
+
+    response = None
 
     try:
         response = session.get(url, params=params, timeout=(3, 30))
-    except requests.exceptions.Timeout as e:
+    except Exception as e:
         raise e
 
     if response.status_code != 200:
-        raise requests.exceptions.RequestException("[Error] API 요청에 실패했습니다. (%d) -> %s" % (index, addr))
-        #return None
+        raise Exception(
+            "[%d-Error] %s - API 요청에 실패했습니다. (%d) -> %s"
+            % (response.status_code, response.reason, index, addr)
+        )
 
-    response.encoding = 'utf-8'
+    response.encoding = "utf-8"
     result = response.json()
+    status = result["response"]["status"]
 
-    status = result['response']['status']
+    if status == "ERROR":
+        error_code = result["response"]["error"]["code"]
+        error_text = result["response"]["error"]["text"]
+        raise Exception(f"[{error_code}] {error_text} (%d) -> %s" % (index, addr))
+    elif status == "NOT_FOUND":
+        raise requests.exceptions.RequestException(
+            "[Warning] 주소를 찾을 수 없습니다. (%d) -> %s" % (index, addr)
+        )
 
-    if status == 'ERROR':
-        error_code = result['response']['error']['code']
-        error_text = result['response']['error']['text']
-        raise requests.exceptions.RequestException(f"[{error_code}] {error_text} (%d) -> %s" % (index, addr))
-        #return None
-    elif status == 'NOT_FOUND':
-        raise requests.exceptions.RequestException("[Error] 주소를 찾을 수 없습니다. (%d) -> %s" % (index, addr))
-        #return None
-
-    longitude = result['response']['result']['point']['x']
-    latitude = result['response']['result']['point']['y']
-    result: tuple[int, Any, Any] =  (index, latitude, longitude)
-    #print(result)
+    longitude = float(result["response"]["result"]["point"]["x"])
+    latitude = float(result["response"]["result"]["point"]["y"])
+    result = (latitude, longitude)
+    logger.info("%s --> (%s, %s)" % (addr, latitude, longitude))
     return result
 
 
@@ -96,58 +114,72 @@ def geocode_process(df: DataFrame, addr: str, key: str) -> DataFrame:
     data: DataFrame = df.copy()
     size: int = len(data)
     success = 0
-    processes = []
 
-    session = requests.Session()
+    typer.echo("요청 데이터 개수: %d" % size)
 
     typer.echo("------------------------------------------")
 
     with tqdm(total=size, colour="yellow") as pbar:
-        with futures.ThreadPoolExecutor(max_workers=10) as executor:
-            for i in range(size):
-                address: str = str(data.loc[i, addr]).strip()
+        with requests.Session() as session:
+            with futures.ThreadPoolExecutor(max_workers=30) as executor:
+                for i in range(size):
+                    time.sleep(0.1)
+                    address: str = str(data.loc[i, addr]).strip()
 
-                if address == 'nan':
-                    pbar.update(1)
-                    data.loc[i, 'latitude'] = None
-                    data.loc[i, 'longitude'] = None
-                    continue
-
-                processes.append(
-                    executor.submit(
+                    p = executor.submit(
                         geocode_item, session, index=i, addr=address, key=key
                     )
-                )
 
-            for p in futures.as_completed(processes):
-                try:
-                    result = p.result()
-
-                    if result is not None:
-                        index, latitude, longitude = result
-                        data.loc[index, 'latitude'] = latitude
-                        data.loc[index, 'longitude'] = longitude
+                    try:
+                        result = p.result()
+                        latitude, longitude = result
+                        data.loc[i, "latitude"] = latitude
+                        data.loc[i, "longitude"] = longitude
                         success += 1
-                except Exception as e:
-                    typer.echo(e)
-                    #executor.shutdown(wait=False, cancel_futures=True)
-                finally:
-                    pbar.update(1)
+                    except requests.exceptions.RequestException as re:
+                        # typer.echo(re)
+                        logger.warning(re)
+                        data.loc[i, "latitude"] = None
+                        data.loc[i, "longitude"] = None
+                    except ValueError as ve:
+                        # typer.echo(ve)
+                        logger.warning(ve)
+                        data.loc[i, "latitude"] = None
+                        data.loc[i, "longitude"] = None
+                    except Exception as e:
+                        executor.shutdown(wait=False, cancel_futures=True)
 
+                        if i > 0:
+                            data.iloc[:i].to_excel("진행완료_데이터.xlsx", index=False)
+                            data.iloc[i:].to_excel("진행중_데이터.xlsx", index=False)
+
+                        raise e
+                    finally:
+                        pbar.update(1)
 
     typer.echo("------------------------------------------")
 
-    data['latitude'] = data['latitude'].astype(float)
-    data['longitude'] = data['longitude'].astype(float)
+    data["latitude"] = data["latitude"].astype(float)
+    data["longitude"] = data["longitude"].astype(float)
 
     typer.echo("------------------------------------------")
-    typer.echo(f"총 {size}개의 데이터 중 {success}개의 데이터가 성공적으로 처리되었습니다.")
+    typer.echo(
+        f"총 {size}개의 데이터 중 {success}개의 데이터가 성공적으로 처리되었습니다."
+    )
 
     return data
 
+
 @app.command()
-def main(key: str = "25DE1852-F870-357A-AF47-169CE76AA841", input: str = typer.Option(), output: str = None, addr: str = '도로명') -> None:
-    #def main(key: str = typer.Option(), input: str = typer.Option(), output: str = None, addr: str = 'ADDR') -> None:
+def main(
+    # key: str = "25DE1852-F870-357A-AF47-169CE76AA841",
+    key: str = "E0B70A87-4BB5-3081-8CC8-23B42575DB15",
+    # key: str = "ABD3E6CE-9D11-3C9A-8E58-9D3A3499D1EF",
+    input: str = typer.Option(),
+    output: str = None,
+    addr: str = "도로명",
+) -> None:
+    # def main(key: str = typer.Option(), input: str = typer.Option(), output: str = None, addr: str = 'ADDR') -> None:
     """
     -----------\n
     브이월드 OpenAPI와 연동하여 GeoCoding을 수행하고 결과를 파일로 저장합니다.\n \n
@@ -160,11 +192,14 @@ def main(key: str = "25DE1852-F870-357A-AF47-169CE76AA841", input: str = typer.O
     -----------\n
     """
 
-    input, output = check_path(input=input, output=output)
-    df: DataFrame = get_dataframe(input=input, addr=addr)
-    result: DataFrame = geocode_process(df=df, addr=addr, key=key)
-
-    result.to_excel(output, index=False)
+    try:
+        input, output = check_path(input=input, output=output)
+        df: DataFrame = get_dataframe(input=input, addr=addr)
+        result: DataFrame = geocode_process(df=df, addr=addr, key=key)
+        result.to_excel(output, index=False)
+    except Exception as e:
+        logger.error(e)
+        typer.echo(e)
 
 
 if __name__ == "__main__":
